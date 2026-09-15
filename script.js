f7defacf3eb8a179f3a152a82f66c87d8cff7991
Illia Blahun <illyablagun@gmail.com>
2026-09-15 11:29:14 +0300

    Initial project setup

---
+++ script.js
@@ -0,0 +1,8 @@
+const sonad = [
+    { eesti: "kass", vene: "кошка" },
+    { eesti: "koer", vene: "собака" },
+    { eesti: "maja", vene: "дом" },
+    { eesti: "auto", vene: "машина" }
+];
+
+console.log(sonad);
